c276c7ad9a0f064e645d50ce6b2baf4414dcd937
Artemios Triantafyllou <atriantafyllou2@tuc.gr>
2026-01-29 13:08:19 +0200

    Correction to benchmark.

--- ft_libs/bench/relational/rhj_narrow/benchmarking.sh
+++ ft_libs/bench/relational/rhj_narrow/benchmarking.sh
@@ -3,13 +3,13 @@
 # 2 joins, with both ftHashJoin and ftHashJoin_old
 
 if [ "$1" ] && [ "$1" -eq 0 ]; then
-	futhark bench --backend=c datagen.fut
+	futhark bench --backend=cuda datagen.fut
 	mv 'data/datagen:rhj0_8-#0_("33554432i64").out' data/data8.in
 	mv 'data/datagen:rhj0_12-#0_("33554432i64").out' data/data12.in
 	mv 'data/datagen:rhj0_16-#0_("33554432i64").out' data/data16.in
 elif [ "$1" ] && [ "$1" -eq 1 ]; then
-	futhark bench --backend=c --runs=10 test_join1.fut
-	futhark bench --backend=c --runs=10 test_join2.fut
+	futhark bench --backend=cuda --runs=10 test_join1.fut
+	futhark bench --backend=cuda --runs=10 test_join2.fut
 	rm -f data/*
 else
 	sh benchmarking.sh 0
