088e7c46ead844943e3d64c994a7e418450f987c
Artemios Triantafyllou <atriantafyllou2@tuc.gr>
2026-02-16 13:23:12 +0200

    fixed typo

--- ft_libs/bench/joins/sort/benchmarking.sh
+++ ft_libs/bench/joins/sort/benchmarking.sh
@@ -8,5 +8,5 @@ payload_bytes=$2
 
 futhark dataset -b -g \[$num_elems\]i32 -b -g \[$num_elems\]\[$payload_bytes\]u8 > data/dat_i32.in
 futhark dataset -b -g \[$num_elems\]i64 -b -g \[$num_elems\]\[$payload_bytes\]u8 > data/dat_i64.in
-futhark bench test_sort.fut --backend=$futhark_backend runs=$num_runs
+futhark bench test_sort.fut --backend=$futhark_backend --runs=$num_runs
 rm -f data/*
