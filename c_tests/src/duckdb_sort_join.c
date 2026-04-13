22b367bdf259179c5bb30d310d47a053cf6c7726
Artemios Triantafyllou <atriantafyllou2@tuc.gr>
2026-04-13 18:31:00 +0300

    Started making some simple scripts for duckdb benchmarks.
    TODO eventually
    
    1. add get-opt parameterisation, & make benchmarks for other cases & combination with futhark.
    
    2. reorganize makefile (& README).
    
    3. make sure these measurements correspond to actual query execution time.

---
+++ c_tests/src/duckdb_sort_join.c
@@ -0,0 +1,146 @@
+// Benchmark sort & join for tables R_tbl, S_tbl using duckdb.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../clibs/duckdb.h"
+
+#include "../../clibs/mylogger.h"
+
+// TODO use getopt to set parameters
+#include <unistd.h>
+#include <getopt.h>
+
+#define R_name "R_tbl"
+#define S_name "S_tbl"
+
+#define k_name "k"
+
+#define default_LOGFILE "stdout"
+#define default_DBFILE "testdb.db"
+
+int main(int argc, char *argv[]) {
+	// TODO use getopt to set these values
+
+		const char *LOGFILE = default_LOGFILE;
+		const char *DBFILE  = default_DBFILE;
+
+	// init logger
+
+		FILE* logfile = loginit(LOGFILE, "Starting program to create tables for join.");
+	    if(LOGFILE && !logfile) {
+	      perror("Failed to initialise logger.\n");
+	      return -1;
+	    }
+
+	// make duckdb connection
+
+	    duckdb_database db;
+	  	duckdb_connection con;
+
+	  	if (duckdb_open(DBFILE, &db) == DuckDBError) {
+	  		perror("Failed to open database.\n");
+	  		return -1;
+	  	}
+	  	if (duckdb_connect(db, &con) == DuckDBError) {
+	  		perror("Failed to connect to database.\n");
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Connected to duckdb database.");
+
+	// Allocate strings for the queries.
+
+	  	char fetch_R[2*strlen(R_name) + 250];
+	  	char fetch_S[2*strlen(S_name) + 250];
+
+	  	char joinQ[2*strlen(R_name)+2*strlen(S_name)+3*strlen(k_name)+250];
+
+	  	char sort_R[2*strlen(R_name) + 250];
+	  	char sort_S[2*strlen(S_name) + 250];
+
+	  	char joinSQ[2*strlen(R_name)+2*strlen(S_name)+3*strlen(k_name)+250];
+
+	// Fetch tables into memory.
+
+	  	sprintf(fetch_R,"CREATE TEMP TABLE %s_tmp AS (FROM %s);", R_name, R_name);
+	  	if(duckdb_query(con, fetch_R, NULL) == DuckDBError) {
+	  		perror("Failed to fetch R into memory.\n");
+	  		perror(fetch_R);
+	  		return -1;
+	  	}
+
+	  	sprintf(fetch_S,"CREATE TEMP TABLE %s_tmp AS (FROM %s);", S_name, S_name);
+	  	if(duckdb_query(con, fetch_S, NULL) == DuckDBError) {
+	  		perror("Failed to fetch S into memory.\n");
+	  		perror(fetch_S);
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Fetched tables into memory.");
+
+	// Join unsorted tables
+
+	  	mylog(logfile, "Performing Inner Equi-Join on unsorted tables...");
+
+	  	sprintf(joinQ,
+	  		"SELECT a.*, b.* EXCLUDE(%s) FROM %s_tmp a JOIN %s_tmp b ON a.%s = b.%s;"
+	  		, k_name, R_name, S_name, k_name, k_name);
+	  	if(duckdb_query(con, joinQ, NULL) == DuckDBError) {
+	  		perror("Failed to perform Equi-Join on unsorted tables.\n");
+	  		perror(joinQ);
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Completed Inner Equi-Join on unsorted tables.");
+
+
+	// Sort tables.
+
+	  	mylog(logfile, "Sorting table R...");
+
+	  	sprintf(sort_R,"CREATE TEMP TABLE %s_sorted AS (FROM %s_tmp ORDER BY %s);", R_name, R_name, k_name);
+	  	if(duckdb_query(con, sort_R, NULL) == DuckDBError) {
+	  		perror("Failed to sort R.\n");
+	  		perror(sort_R);
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Sorted table R.");
+
+	  	mylog(logfile, "Sorting table S...");
+
+	  	sprintf(sort_S,"CREATE TEMP TABLE %s_sorted AS (FROM %s_tmp ORDER BY %s);", S_name, S_name, k_name);
+	  	if(duckdb_query(con, sort_S, NULL) == DuckDBError) {
+	  		perror("Failed to sort S.\n");
+	  		perror(sort_S);
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Sorted table S.");
+
+	// Join unsorted tables
+
+	  	mylog(logfile, "Performing Inner Equi-Join on sorted tables...");
+
+	  	sprintf(joinSQ,
+	  		"SELECT a.*, b.* EXCLUDE(%s) FROM %s_sorted a JOIN %s_sorted b ON a.%s = b.%s;"
+	  		, k_name, R_name, S_name, k_name, k_name);
+	  	if(duckdb_query(con, joinSQ, NULL) == DuckDBError) {
+	  		perror("Failed to perform Equi-Join on sorted tables.\n");
+	  		perror(joinSQ);
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Completed Inner Equi-Join on sorted tables.");
+
+	 // Cleanup
+
+	  	duckdb_disconnect(&con);
+		duckdb_close(&db);
+  		mylog(logfile, "Disconnected duckdb and freed its memory.");
+
+  		logclose(logfile);
+  
+  return 0;
+}
---
+++ c_tests/src/mk_tbls_wide.c
@@ -0,0 +1,181 @@
+// Make tables R_tbl, S_tbl, for narrow & wide joins tests.
+
+// TODO make sure the time measurements correspong to actual query execution
+// and not just time to launch the query or some other meaningless thing
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../clibs/duckdb.h"
+
+#include "../../clibs/mylogger.h"
+
+// TODO use getopt to set parameters
+#include <unistd.h>
+#include <getopt.h>
+
+#define R_name "R_tbl"
+#define S_name "S_tbl"
+
+#define default_R_size 1000000
+#define default_S_size 2000000
+
+#define default_R_vals 1000000
+#define default_S_vals 1000000
+
+#define default_k_type "INTEGER"
+#define default_pL_type "INTEGER"
+
+#define k_name "k"
+#define pL_name "pL"
+
+#define default_num_pL 2
+
+#define default_LOGFILE "stdout"
+#define default_DBFILE "testdb.db"
+
+int main(int argc, char *argv[]) {
+	// TODO use getopt to set these values
+
+		int64_t R_size = default_R_size;
+		int64_t S_size = default_S_size;
+
+		int64_t R_vals = default_R_vals;
+		int64_t S_vals = default_S_vals;
+
+		const char *k_type  = default_k_type;
+		const char *pL_type = default_pL_type;
+
+		int64_t num_pL = default_num_pL;
+
+		const char *LOGFILE = default_LOGFILE;
+		const char *DBFILE  = default_DBFILE;
+
+	// init logger
+
+		FILE* logfile = loginit(LOGFILE, "Starting program to create tables for join.");
+	    if(LOGFILE && !logfile) {
+	      perror("Failed to initialise logger.\n");
+	      return -1;
+	    }
+
+	// TODO log parameters
+
+    // make duckdb connection
+
+	    duckdb_database db;
+	  	duckdb_connection con;
+
+	  	if (duckdb_open(DBFILE, &db) == DuckDBError) {
+	  		perror("Failed to open database.\n");
+	  		return -1;
+	  	}
+	  	if (duckdb_connect(db, &con) == DuckDBError) {
+	  		perror("Failed to connect to database.\n");
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Connected to duckdb database.");
+
+	// Allocate strings for the queries.
+
+	  	char R_primary[50] = "";
+	  	if(R_vals >= R_size) {
+	  		sprintf(R_primary, "PRIMARY KEY");
+	  	}
+
+	  	char query_prepare_R[strlen(k_name)+strlen(R_name)+strlen(k_type)+500];
+	  	char query_prepare_S[strlen(k_name)+strlen(R_name)+strlen(k_type)+500];
+
+	  	char query_mk_R[strlen(k_name)+strlen(R_primary)+strlen(k_type)+num_pL*(strlen(pL_name)+strlen(pL_type)+5)+strlen(R_name)+500];
+	  	char query_mk_S[strlen(k_name)+strlen(k_type)+num_pL*(strlen(pL_name)+strlen(pL_type)+5)+strlen(S_name)+500];
+
+	  	char query_load_R[2*strlen(R_name)+500+25*num_pL];
+	  	char query_load_S[2*strlen(R_name)+500+25*num_pL];
+
+  	// -- -- -- Create pre-shuffled keys.
+
+	  	sprintf(query_prepare_R,
+	  		"CREATE OR REPLACE TEMP TABLE %s_prep AS \n(SELECT i %% %ld, random()*(10*%ld) FROM range(%ld) t(i));"
+	  		, R_name, R_vals, R_size, R_size);
+	  	sprintf(query_prepare_S,
+	  		"CREATE OR REPLACE TEMP TABLE %s_prep AS \n(SELECT i %% %ld, random()*(10*%ld) FROM range(%ld) t(i));"
+	  		, S_name, S_vals, S_size, S_size);
+
+	  	if(duckdb_query(con, query_prepare_R, NULL) == DuckDBError) {
+	  		perror("Failed to prepare table R.\n");
+	  		perror(query_prepare_R);
+	  		return -1;
+	  	}
+	  	if(duckdb_query(con, query_prepare_S, NULL) == DuckDBError) {
+	  		perror("Failed to prepare table S.\n");
+	  		perror(query_prepare_S);
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Created unshuffled data.");
+
+  	// -- -- -- Create tables where keys will be shuffled.
+
+	  	int len_R1 = sprintf(query_mk_R, "CREATE OR REPLACE TABLE %s (%s %s %s", R_name, k_name, k_type, R_primary);
+	  	for(int64_t i=0; i<num_pL; i++) {
+	  		len_R1 += sprintf(query_mk_R+len_R1, ", %s_%ld %s", pL_name, i, pL_type);
+	  	}
+	  	sprintf(query_mk_R+len_R1, ");");
+
+	  	int len_S1 = sprintf(query_mk_S, "CREATE OR REPLACE TABLE %s (%s %s", S_name, k_name, k_type);
+	  	for(int64_t i=0; i<num_pL; i++) {
+	  		len_S1 += sprintf(query_mk_S+len_S1, ", %s_%ld %s", pL_name, i, pL_type);
+	  	}
+	  	sprintf(query_mk_S+len_S1, ");");
+
+	  	if(duckdb_query(con, query_mk_R, NULL) == DuckDBError) {
+	  		perror("Failed to create table R.\n");
+	  		perror(query_mk_R);
+	  		return -1;
+	  	}
+	  	if(duckdb_query(con, query_mk_S, NULL) == DuckDBError) {
+	  		perror("Failed to create table S.\n");
+	  		perror(query_mk_S);
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Created tables in the database (currently empty).");
+
+  	// -- -- -- Shuffle keys & append random payloads.
+
+  		int len_R2 = sprintf(query_load_R, "INSERT INTO %s \nSELECT #1", R_name);
+  		for(int64_t i=0; i<num_pL; i++) {
+  			len_R2 += sprintf(query_load_R+len_R2,", 255*random()");
+  		}
+  		sprintf(query_load_R+len_R2," FROM (FROM %s_prep ORDER BY #2);", R_name);
+
+  		int len_S2 = sprintf(query_load_S, "INSERT INTO %s \nSELECT #1", S_name);
+  		for(int64_t i=0; i<num_pL; i++) {
+  			len_S2 += sprintf(query_load_S+len_S2,", 255*random()");
+  		}
+  		sprintf(query_load_S+len_S2," FROM (FROM %s_prep ORDER BY #2);", S_name);
+
+  		if(duckdb_query(con, query_load_R, NULL) == DuckDBError) {
+	  		perror("Failed to load data into R.\n");
+	  		perror(query_load_R);
+	  		return -1;
+	  	}
+	  	if(duckdb_query(con, query_load_S, NULL) == DuckDBError) {
+	  		perror("Failed to load data into S.\n");
+	  		perror(query_load_S);
+	  		return -1;
+	  	}
+
+	  	mylog(logfile, "Loaded shuffled keys & random payloads into the tables.");
+
+	// Cleanup
+
+	  	duckdb_disconnect(&con);
+		duckdb_close(&db);
+  		mylog(logfile, "Disconnected duckdb and freed its memory.");
+
+  		logclose(logfile);
+  
+  return 0;
+}
